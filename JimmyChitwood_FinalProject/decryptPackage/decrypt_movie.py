61a1b1d40bdfb52abf04220fa575cf0fec1aaf92
jldriehaus <driehajl@mail.uc.edu>
2025-04-22 09:01:15 -0400

    Added more documentation to explain the code in decrpyt_movie

--- JimmyChitwood_FinalProject/decryptPackage/decrypt_movie.py
+++ JimmyChitwood_FinalProject/decryptPackage/decrypt_movie.py
@@ -17,17 +17,43 @@ import json
 from cryptography.fernet import Fernet
 
 class MovieDecryptor:
+    """
+    A class that handles decryption of an encrypted movie title for a given group using Fernet symmetric encryption.
+    """
     def __init__(self, encrypted_file_path):
+        """
+        Initializes the MovieDecryptor with the path to the encrypted JSON file and sets up the Fernet object
+        using the provided group key.
+
+        Args: 
+            encrypted_file_path (str): Path to the JSON file containing encrypted movie titles keyed by group name.
+        """
         self.encrypted_file_path = encrypted_file_path
+        # Fernet key used for decryption
         self.fernet_key = b"GusN5ceicQjGeKNr0gedUkjZ6h4I8xXm6Thx_issRko="
+        # Fernet object handles the encryption/decryption logic
         self.fernet = Fernet(self.fernet_key)
 
     def decrypt_movie(self, group_name):
+        """
+        Decrypts the movie title associated with the given group name.
+
+        Args:
+            group_name (str): The name of the group to look up in the JSON file.
+
+        Returns:
+            str: The decrypted movie title if found and successfully decrypted;
+                 otherwise, returns an error message.
+        """
+        # Load the encrypted movie titles from the JSON file
         with open(self.encrypted_file_path, 'r') as f:
             encrypted_data = json.load(f)
-
+        
+        # Retrieve the encrypted message for the group
         encrypted_message = encrypted_data.get(group_name, [None])[0]
+
         if encrypted_message:
+            # Decode and decrypt the message using the Fernet key
             decrypted_bytes = self.fernet.decrypt(encrypted_message.encode())
             return decrypted_bytes.decode()
         else:
